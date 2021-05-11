1a877270fba055fae3b4b1f98e48f297a0cce795
marcellanywi <marcell.marosvolgyi@anywi.com>
2021-05-11 14:34:13 +0200

    sandbox stuff

---
+++ sandbox/sets.py
@@ -0,0 +1,18 @@
+"""
+    Author: M.A. Marosvolgyi 2021
+
+    These are just some thoughts and experiments:
+
+    Attempt to help mapping by finding elements (primitives) which then
+    are categorized in different sets:
+    e.g.:
+
+    elements:  assets, fw update, monitoring, configurations,
+               {personal, device, calibration, sw}data, password
+
+    sets: transfer, management, software integrity, privacy,
+          authenitcity, attack surface, 
+
+"""
+
+
